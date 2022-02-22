eb46b705e33b6d5f92180f6808fcac956de4efb4
Andres01Perez <andresperez288233@correo.itm.edu.co>
2022-02-22 16:09:23 -0500

    finish Supermarket (first proyect)

--- Supermarket/Supermarket/Invoice.cs
+++ Supermarket/Supermarket/Invoice.cs
@@ -1,16 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Supermarket
+﻿namespace Supermarket
 {
     public class Invoice : IPay
     {
+
+        List<Product> _products = new List<Product>();
+
+        public Invoice()
+        {
+        }
+        public void AddProduct(Product product1)
+        {
+            _products.Add(product1);
+        }
         public decimal ValueToPay()
         {
-            throw new NotImplementedException();
+            decimal total = 0;
+            foreach (Product prod in _products)
+            {
+                total += prod.ValueToPay();
+            }
+            return total;
         }
+        public override string ToString()
+        {
+
+            string payroll = "";
+            foreach (Product prod in _products)
+            {
+
+                payroll += prod.ToString() + "\n";
+            }
+
+            return $"{payroll}                 ================\n" +
+                $"TOTAL:         {$"{ValueToPay():C2}",18}";
+        }
+
     }
 }
--- Supermarket/Supermarket/Program.cs
+++ Supermarket/Supermarket/Program.cs
@@ -47,6 +47,9 @@ Console.WriteLine(product3);
 Console.WriteLine(product4);
 Console.WriteLine(product5);
 
+Console.WriteLine("RECEIPT");
+Console.WriteLine("-------------------------------------------------");
+
 Invoice invoice = new Invoice();
 invoice.AddProduct(product1);
 invoice.AddProduct(product3);
